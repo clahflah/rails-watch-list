75f24c6f0a1a38a1e4344b4b25840bcabcf422a5
Clayton Flaherty <claytonflaherty92@gmail.com>
2022-08-20 10:31:37 +0200

    functionality complete working on CSS

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'open-uri'
+require 'json'
+Movie.destroy_all
+
+
+uri = URI.open('https://tmdb.lewagon.com/movie/top_rated')
+json = JSON.parse(uri.read)
+json["results"].each do |movie|
+  Movie.create!(
+    title: movie['original_title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
+    rating: movie['vote_average']
+  )
+end
